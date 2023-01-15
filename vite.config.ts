ef54fc8c65be49eaf8f245cdde4f305d58a86894
KrystianKasp98 <krystiankasprzak98byd@gmail.com>
2023-01-16 00:43:24 +0100

    add test

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 
-// add .env, add eslint
-
-export default defineConfig(({command, mode}) => {
-  return {
-    plugins: [react()],
-  };
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), dts()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+  }
 });
