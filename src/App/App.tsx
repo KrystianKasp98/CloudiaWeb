aebe75751b41bb0aa66b72ee0582fe3e144b6d8c
KrystianKasp98 <krystiankasprzak98byd@gmail.com>
2023-01-16 00:28:05 +0100

    structure refactor, add scss

---
+++ src/App/App.tsx
@@ -0,0 +1,13 @@
+import { BACKEND_URL } from '../consts';
+import './styles/App.scss';
+
+function App() {
+  console.log(BACKEND_URL);
+  return (
+    <div className="App">
+      <p>hi</p>
+    </div>
+  );
+}
+
+export default App;
