11d6014a04e04ae4c537d277ce81368fa157f331
KrystianKasp98 <krystiankasprzak98byd@gmail.com>
2023-01-16 00:08:51 +0100

    add eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,19 @@
 module.exports = {
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'eslint-config-prettier',
   ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: ["./tsconfig.json"],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
   },
   plugins: ["@typescript-eslint"],
   root: true,
